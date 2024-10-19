4b5eb956ef500ad776b8803cf3a1a047b82c2d07
Rumeleq <mmikolaj0770@gmail.com>
2024-10-19 19:27:44 +0200

    twoja matka
    f

--- js/nav_list.js
+++ js/nav_list.js
@@ -32,12 +32,38 @@ const sfilenames = ['s1.html', 's2.html', 's3.html', 's4.html', 's5.html', 's6.h
 const nspanTexts = ['J. Andrzejak (AJ)', 'A. Brych (AB)', 'K. Chwałowski (KC)', '1. Ckp (CKP1)', '2. Ckp (CKP2)', 'Z. Filipek (ZF)', 'R. Fiweg (RF)', 'R. Garcon (GR)', 'R. Gilewicz (RG)', 'J. Gimżewski (JG)', 'A. Gizelska (AG)', 'K. Giżyński (GI)', 'E. Gostomska (EG)', 'E. Iwaniec (EI)', 'A. Jaborkhel (JA)', 'M. Jadwiszczok (MJ)', 'K. Jakubowska (KJ)', 'J. Janicki (JJ)', 'G. Kaźmierczak (KG)', 'R. Kocik (RK)', 'K. Kościelniak (KK)', 'W. Kowalewski (WK)', 'A. Kozłowski (KZ)', 'S. Kubica (SK)', 'F. Kuczewski (FK)', 'J. Kułaczkowski (JK)', 'K. Lancmańska (KL)', 'G. Ledachowicz (GL)', 'O. Legner (OL)', 'A. Lerczak (AL)', 'A. Lisiecka (LA)', 'C. Lossy (CL)', 'M. Łażewski (MŁ)', 'K. Makowska (KM)', 'O. Malengowska (OM)', 'P. Matuszak (PM)', 'A. Matyjasek (AM)', 'W. Metello-Kasprzyk (ME)', 'R. Mikołajczak (RM)', 'N. Mocek (NM)', 'F. Napierała (FN)', 'M. Nowak (MN)', 'J. Obstalecka (JO)', 'W. Ostrowski (WO)', 'P. Palacz (PP)', 'A. Pietrzak (PA)', 'J. Rabenda (JR)', 'M. Raus (RA)', 'D. Renk (DR)', 'M. Rękoś (MR)', 'W. Rząsa (WR)', 'U. Skrzypek (SU)', 'I. Smierzchalska (IS)', 'L. Sokołowska (LS)', 'M. Sopa (SG)', 'A. Szewczyczak (SE)', 'P. Szulada (SZ)', 'W. Szumski (WSZ)', 'E. Szutowska-Pietranis (EP)', 'J. Szygenda (SJ)', 'A. Szymaniak (AS)', 'P. Szymczak (PS)', 'M. Szyper (MS)', 'D. Szypko (DS)', 'M. Śliwa (SW)', 'E. Tarabasz (TE)', 'J. Tendera (JT)', 'M. Tomczak-Niewiada (MT)', 'R. Tyma (RT)', 'S. Wartacz (WS)', 'M. Wawrzyniak (MW)', 'M. Witczak (WM)', 'P. Witczak (WZ)', 'K. Wojciechowska (KW)', 'A. Wojcieszak (AW)', 'I. Zawal (ZI)'];
 const nfilenames = ['n1.html', 'n2.html', 'n3.html', 'n4.html', 'n5.html', 'n6.html', 'n7.html', 'n8.html', 'n9.html', 'n10.html', 'n11.html', 'n12.html', 'n13.html', 'n14.html', 'n15.html', 'n16.html', 'n17.html', 'n18.html', 'n19.html', 'n20.html', 'n21.html', 'n22.html', 'n23.html', 'n24.html', 'n25.html', 'n26.html', 'n27.html', 'n28.html', 'n29.html', 'n30.html', 'n31.html', 'n32.html', 'n33.html', 'n34.html', 'n35.html', 'n36.html', 'n37.html', 'n38.html', 'n39.html', 'n40.html', 'n41.html', 'n42.html', 'n43.html', 'n44.html', 'n45.html', 'n46.html', 'n47.html', 'n48.html', 'n49.html', 'n50.html', 'n51.html', 'n52.html', 'n53.html', 'n54.html', 'n55.html', 'n56.html', 'n57.html', 'n58.html', 'n59.html', 'n60.html', 'n61.html', 'n62.html', 'n63.html', 'n64.html', 'n65.html', 'n66.html', 'n67.html', 'n68.html', 'n69.html', 'n70.html', 'n71.html', 'n72.html', 'n73.html', 'n74.html', 'n75.html', 'n76.html'];
 
-let navContainer = document.getElementById('nav-container');
-let indexLink = addElement('a', 'nav#nav-container', true);
-indexLink.href = '../planyzsk/index.html';
-indexLink.textContent = 'Strona główna';
-generateList(ospanTexts, ofilenames, navContainer);
-generateList(nspanTexts, nfilenames, navContainer);
-generateList(sspanTexts, sfilenames, navContainer);
+//let parentDocument = window.parent.document;
+//let navIframe = parentDocument.getElementById('nav-list');
+//let scheduleIframe = parentDocument.getElementById('schedule-frame');
 
+//navIframe.onload = function()
+//{
+    let navContainer = document.getElementById('nav-container');
+    let indexLink = addElement('a', 'nav#nav-container', true);
+    indexLink.href = '../planyzsk/index.html';
+    indexLink.textContent = 'Strona główna';
+    generateList(ospanTexts, ofilenames, navContainer);
+    generateList(nspanTexts, nfilenames, navContainer);
+    generateList(sspanTexts, sfilenames, navContainer);
+
+    navContainer.addEventListener('click', function(event)
+    {
+        if (event.target.tagName === 'A')
+        {
+            event.preventDefault();
+            console.log(event.target.textContent);
+            if (event.target.textContent == 'Strona główna')
+                window.parent.location = '../planyzsk/index.html';
+            else
+            {
+                let href = event.target.href;
+                let message = {
+                    action: 'navigate',
+                    href: href
+                };
+                window.parent.postMessage(message, '*');
+            }
+        }
+    });
+//}
 
--- js/plan_index.js
+++ js/plan_index.js
@@ -7,31 +7,21 @@ window.onload = function()
 {
     document.body.style.visibility = "visible";
 
-let navIframe = document.getElementById('nav-list');
-let navContainer = navIframe.contentDocument.getElementById('nav-container');
-
-navContainer.addEventListener('click', function(event)
-{
-    if (event.target.tagName === 'A')
+    let scheduleHref = getQueryParam('schedule');
+    if (scheduleHref) 
     {
-        event.preventDefault();
-        if (event.target.textContent === 'Strona główna')
-            window.location = '../planyzsk/index.html';
-        else
+        let scheduleIframe = document.getElementById('schedule-frame');
+        scheduleIframe.src = scheduleHref;
+    }
+    window.addEventListener('message', function(event) 
+    {
+        if (event.data.action === 'navigate') 
         {
-            let href = event.target.href;
+            let href = event.data.href;
             let parts = href.split('/');
             let newHref = parts.slice(-2).join('/');
-            let iframe = document.getElementById('schedule-frame');
-            iframe.src = newHref;
+            let scheduleIframe = document.getElementById('schedule-frame');
+            scheduleIframe.src = newHref;
         }
-    }
-});
-
-let scheduleHref = getQueryParam('schedule');
-if (scheduleHref) 
-{
-    let scheduleIframe = document.getElementById('schedule-frame');
-    scheduleIframe.src = scheduleHref;
-}
+    });
 }
