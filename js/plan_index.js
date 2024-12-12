dad95647a27e6adab2c5a78457627c2cab436982
Rumeleq <100099007+Rumeleq@users.noreply.github.com>
2024-12-12 18:56:12 +0100

    Update plan_index.js

--- js/plan_index.js
+++ js/plan_index.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function()
     let navContainer = document.getElementById('nav-container');
     let scheduleIframe = document.getElementById('schedule-frame');
     if (window.innerWidth <= 980)
-        switchNav(svg, navContainer, scheduleIframe, true);
+        switchNav(svg, navContainer, scheduleIframe, forceHiddenNav=true);
 
     window.addEventListener('message', function(event)
     {
@@ -70,7 +70,7 @@ document.addEventListener('DOMContentLoaded', function()
 
     //Schowanie nav bara, jeśli jest widoczny, po zmniejszeniu okna przeglądarki
     if (window.innerWidth <= 980)
-        switchNav(svg, navContainer, scheduleIframe, true);
+        switchNav(svg, navContainer, scheduleIframe, forceHiddenNav=true);
     window.addEventListener('resize', () =>
         handleMediaQuery(svg, navContainer, scheduleIframe));
 
