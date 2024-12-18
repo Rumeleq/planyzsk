f27891a08198fc25841663815b1b44eca0560af1
Rumeleq <100099007+Rumeleq@users.noreply.github.com>
2024-12-18 18:16:09 +0100

    normalizacja dodana
    do poprawki template, w następnej kolejności inserty

--- pyscraper/html_parser.py
+++ pyscraper/html_parser.py
@@ -1,8 +1,33 @@
 import json
 import os
+import re
+
 from bs4 import BeautifulSoup as bs
+from utils.constants import WORDS_TO_ADD_SPACE_AFTER
+from utils.constants import WORDS_TO_ADD_SPACE_AFTER_FIRST_CHARACTER
+from utils.constants import WORDS_TO_ADD_SPACE_AFTER_SECOND_CHARACTER
 from utils.constants import HTML_PATH
 from utils.constants import JSON_PATH
+
+
+def normalize_lesson_name(lesson_name: str) -> str:
+    """
+       Normalize lesson name by adding spaces according to predefined rules.
+
+       Args:
+           lesson_name (str): The original lesson name to normalize.
+
+       Returns:
+           str: Normalized lesson name with appropriate spacing added.
+   """
+    for word in WORDS_TO_ADD_SPACE_AFTER:
+        lesson_name = re.sub(rf'(?i){re.escape(word)}', f'{word} ', lesson_name)
+    for word in WORDS_TO_ADD_SPACE_AFTER_FIRST_CHARACTER:
+        lesson_name = re.sub(rf'(?i){re.escape(word)}', f'{word[0]} {word[1:]}', lesson_name)
+    for word in WORDS_TO_ADD_SPACE_AFTER_SECOND_CHARACTER:
+        lesson_name = re.sub(rf'(?i){re.escape(word)}', f'{word[:2]} {word[2:]}', lesson_name)
+
+    return lesson_name
 
 
 def parse_json_to_html():
@@ -22,8 +47,10 @@ def parse_json_to_html():
             if school_hour is None:
                 continue
             for lesson in school_hour:
-                subject, teacher, room = lesson
-                print(f'{subject} {teacher} {room}')
+                subject = lesson[0]
+                lesson_name = normalize_lesson_name(subject)
+                lesson[0] = lesson_name
+                print(lesson)
 
     with open('test.html', 'w', encoding='utf-8') as f:
         f.write(soup.prettify())
--- pyscraper/utils/constants.py
+++ pyscraper/utils/constants.py
@@ -7,6 +7,10 @@ WEEK_DAYS = 5
 LESSONS_NUMBER = 11
 HTML_PATH = 'utils'
 
+WORDS_TO_ADD_SPACE_AFTER = ['Język', 'Rysunek', 'Pracownia', 'Tworzenie', 'aplikacji', 'Biznes', 'Zajęcia', 'Systemy', 'Wychowanie', 'Programowanie', 'baz', 'programowania', 'Projektowanie', 'Eksploatacja', 'handlowa', 'Działalność', 'usługowa', 'Testowanie', 'dokumentowanie', 'Technika', 'ruchu', 'projektowania', 'danych', 'Historia', 'fizyczne', 'internetowych', 'Informatyka', 'obiektowego', 'angielski', 'Podstawy', 'hiszpański', 'niemiecki', 'Elektrotechnika', 'Urządzenia', 'instalacje', 'urządzeń', 'montażu', 'Układy', 'elektryczne', 'elektryczna', 'Bezpieczeństwo', 'higiena', 'techniki', 'systemów', 'sieci', 'witryn', 'Edukacja', 'dla', 'podstaw', 'utrzymania', 'Pomiary', 'Sieci', 'podstacje', 'trakcyjne', 'elektroniczne', 'instalacji', 'Maszyny', 'urządzenia', 'testowania', 'dokumentowania', 'Kompetencje', 'Witryny', 'aplikacje', 'automatyki', 'Technologie', 'pomiary', 'DSDI', 'Instalacje', 'Wiedza', 'społeczeństwie', 'Przygotowanie', 'praktyczne', 'zawodu', 'maszynisty', 'Organizacja', 'przewozów', 'kolejowych', 'Odprawa', 'pociągów', 'stacji', 'DSD', 'Infrastruktura', 'komputerowe', 'technologii', 'pomiarów', 'sterowania', 'ruchem', 'Bazy', 'Kolejowe', 'pojazdy', 'taboru', 'Tabor', 'elektrycznych', 'pojazdów', 'eksploatacji', 'środków', 'transportu', 'organizacji', 'pracy']
+WORDS_TO_ADD_SPACE_AFTER_FIRST_CHARACTER = ['zwychowawcą', 'izarządzanie', 'iurządzeń', 'idokumentowanie', 'ielektronika', 'ielektroniczna', 'iinstalacje', 'ihigiena', 'ielektroniczne', 'iutrzymania', 'ipodstacje', 'iurządzenia', 'idokumentowania', 'iaplikacje', 'wautomatyce', 'ipomiary', 'ospołeczeństwie', 'ipomiarów']
+WORDS_TO_ADD_SPACE_AFTER_SECOND_CHARACTER = ['dozawodu', 'nastacji']
+
 with open(f'{JSON_PATH}lessons.json', 'r', encoding='utf-8') as f:
     LESSONS: dict[str, str] = json.load(f)  # Constant to replace corrupted lesson names {corrupted_lesson: lesson_name}
 with open(f'{JSON_PATH}teachers.json', 'r', encoding='utf-8') as f:
