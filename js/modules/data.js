ed40680a5018b43739c76cea8f3c1d3c2d62b159
Rumeleq <100099007+Rumeleq@users.noreply.github.com>
2025-01-16 18:33:22 +0100

    forgot to update paths in js file maps

--- js/modules/data.js
+++ js/modules/data.js
@@ -1,3 +1,5 @@
+const JSON_PATH = '/planyzsk/pyscraper/JSON/resources/';
+
 async function loadJSON(url)
 {
     const response = await fetch(url);
@@ -6,37 +8,37 @@ async function loadJSON(url)
 
 export async function getOspanTexts()
 {
-    const data = await loadJSON('/planyzsk/pyscraper/JSON/o_map.json');
+    const data = await loadJSON(`${JSON_PATH}o_map.json`);
     return Object.keys(data);
 }
 
 export async function getOfilenames()
 {
-    const data = await loadJSON('/planyzsk/pyscraper/JSON/o_map.json');
+    const data = await loadJSON(`${JSON_PATH}o_map.json`);
     return Object.values(data).map(value => `${value}.html`);
 }
 
 export async function getSspanTexts()
 {
-    const data = await loadJSON('/planyzsk/pyscraper/JSON/s_map.json');
+    const data = await loadJSON(`${JSON_PATH}s_map.json`);
     return Object.keys(data).map(key => /^\d{2}$/.test(key) ? `s${key}` : key);
 }
 
 export async function getSfilenames()
 {
-    const data = await loadJSON('/planyzsk/pyscraper/JSON/s_map.json');
+    const data = await loadJSON(`${JSON_PATH}s_map.json`);
     return Object.values(data).map(value => `${value}.html`);
 }
 
 export async function getNspanTexts()
 {
-    const nMap = await loadJSON('/planyzsk/pyscraper/JSON/n_map.json');
-    const initialsDict = await loadJSON('/planyzsk/pyscraper/JSON/initials_name_dict.json');
+    const nMap = await loadJSON(`${JSON_PATH}n_map.json`);
+    const initialsDict = await loadJSON(`${JSON_PATH}initials_name_dict.json`);
     return Object.keys(nMap).map(key => `${initialsDict[key]} (${key})`);
 }
 
 export async function getNfilenames()
 {
-    const data = await loadJSON('/planyzsk/pyscraper/JSON/n_map.json');
+    const data = await loadJSON(`${JSON_PATH}n_map.json`);
     return Object.values(data).map(value => `${value}.html`);
 }
