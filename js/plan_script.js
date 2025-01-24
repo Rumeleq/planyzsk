376e0c0bee7f9fbe7caa08773f2f7f0823299e6a
JSiwy <siwy.jann@gmail.com>
2025-01-24 10:01:21 +0100

    do zmiany: svg gwiazdki dodać zmiane tła gwiazdki jeśli link istnieje w local storage (jasna/ciemna)(jest dodana/nie jest) gdzieś rozsądnie dać gwiazdkę

--- js/plan_script.js
+++ js/plan_script.js
@@ -23,30 +23,43 @@ document.addEventListener('DOMContentLoaded', function()
     document.body.style.visibility = 'visible';
     window.parent.postMessage({msg_type: title.textContent.trim()}, '*');
 
+    let plan_name = document.querySelector('span:first-of-type');
+    plan_name.id = 'plan_name';
+
     let fav_map = new Map(JSON.parse(localStorage.getItem("fav_plans") || "[]"));
-
     let parentUrl = window.location !== window.parent.location ? document.referrer : document.URL;
-    let add_buton = document.createAttribute('button');
-    add_buton.onclick = appendToStorage;
-    if(fav_map.has(parentUrl)) {
-        add_buton.textContent = 'Usuń z ulubionych';
-    }
-    else {
-        add_buton.textContent = 'Dodaj do ulubionych';
-    }
+    parentUrl = parentUrl.substring(0, parentUrl.lastIndexOf('/'));
+    let iframeSrc = parent.document.getElementById('schedule-frame').src;
+    let lastPart = iframeSrc.substring(iframeSrc.lastIndexOf('/') + 1);
+    let fullUrl = `${parentUrl}/${lastPart}`;
 
+    let add_cbox = document.createElement('input');
+    add_cbox.type = 'checkbox';
+    add_cbox.id = 'add-to-fav';
+    add_cbox.name = 'add-cbox';
+    add_cbox.onclick = appendToStorage;
+    add_cbox.checked = fav_map.has(fullUrl);
+    document.body.appendChild(add_cbox);
 
-
-
+    let cbox_label = document.createElement('label');
+    cbox_label.htmlFor = 'add-cbox';
+    cbox_label.textContent = 'Dodaj do ulubionych';
+    document.body.appendChild(cbox_label);
 });
 
 window.appendToStorage = () => {
     let fav_map = new Map(JSON.parse(localStorage.getItem("fav_plans") || "[]"));
     let parentUrl = window.location !== window.parent.location ? document.referrer : document.URL;
-    if(!fav_map.has(parentUrl)) {
-        fav_map.set(parentUrl, plan_name.innerText);
+    parentUrl = parentUrl.substring(0, parentUrl.lastIndexOf('/'));
+    let iframeSrc = parent.document.getElementById('schedule-frame').src;
+    let lastPart = iframeSrc.substring(iframeSrc.lastIndexOf('/') + 1);
+    let fullUrl = `${parentUrl}/${lastPart}`;
+
+    let plan_name = document.getElementById('plan_name');
+    if(!fav_map.has(fullUrl)) {
+        fav_map.set(fullUrl, plan_name.innerText);
     } else {
-        fav_map.delete(parentUrl);
+        fav_map.delete(fullUrl);
     }
-    localStorage.setItem("fav_plans", JSON.stringify(Array.from(fav_map.entries())));
-}
+        localStorage.setItem("fav_plans", JSON.stringify(Array.from(fav_map.entries())));
+    }
