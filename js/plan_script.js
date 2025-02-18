c4331a78afd013e7d523e30598a0e165f73e9116
Rumeleq <100099007+Rumeleq@users.noreply.github.com>
2025-02-18 17:41:44 +0100

    formatting + added id to schedule template

--- js/plan_script.js
+++ js/plan_script.js
@@ -2,7 +2,7 @@ import { checkCtrlD, addElement } from './modules/utils.js';
 
 document.addEventListener('DOMContentLoaded', function()
 {
-    const urlBase = '../planyzsk/plan_index.html?schedule=dane';
+    const base_url = '../planyzsk/plan_index.html?schedule=dane';
 
     let title = document.querySelector('title');
     document.addEventListener('keydown', function(event) 
@@ -18,20 +18,12 @@ document.addEventListener('DOMContentLoaded', function()
     if (kumi_gaming_anchor !== null)
         kumi_gaming_anchor.addEventListener('click', createTwitchEmbed);
 
-
-    //sekcja odpowiedzialna za dodawanie do ulubionych
-
-    //Dodanie id do nazwy planu
-    let plan_name = document.querySelector('span:first-of-type');
-    plan_name.id = 'plan_name';
-
-
     //Stworzenie url do sprawdzenia czy plan jest w ulubionych
     let fav_map = new Map(JSON.parse(localStorage.getItem('fav_plans') || '[]'));
 
     let documentContent = parent.document.getElementById('schedule-frame').contentDocument.URL;
-    let lastPart = documentContent.substring(documentContent.lastIndexOf('/') + 1);
-    let fullUrl = `${urlBase}/${lastPart}`;
+    let last_part = documentContent.substring(documentContent.lastIndexOf('/') + 1);
+    let full_url = `${base_url}/${last_part}`;
 
     //Dodanie checkboxa do dodawania do ulubionych (jeśli plan jest w ulubionych to checkbox jest zaznaczony)
     let add_cbox = document.createElement('input');
@@ -41,18 +33,16 @@ document.addEventListener('DOMContentLoaded', function()
     add_cbox.onclick = appendToStorage;
     add_cbox.title = 'Dodaj do ulubionych';
     add_cbox.checked = false;
-    if (fav_map.has(fullUrl)) {
+    if (fav_map.has(full_url))
+    {
         add_cbox.checked = true;
         add_cbox.title = 'Usuń z ulubionych';
     }
-    document.getElementById('plan_name').appendChild(add_cbox);
+    document.getElementById('schedule-name').appendChild(add_cbox);
 
     //Wysyłanie title strony do parenta (plan_index) i ustawienie widoczności strony
     document.body.style.visibility = 'visible';
     window.parent.postMessage({msg_type: title.textContent.trim()}, '*');
-
-//Funkcja dodająca/usuwajaca plan do ulubionych
-
 
     //Wysyłanie title strony do parenta (plan_index) i ustawienie widoczności strony
     document.body.style.visibility = 'visible';
@@ -60,29 +50,28 @@ document.addEventListener('DOMContentLoaded', function()
 
 });
 
+//Funkcja dodająca/usuwajaca plan do ulubionych
 window.appendToStorage = () =>
 {
-    const urlBase = '../planyzsk/plan_index.html?schedule=dane';
+    const url_base = '../planyzsk/plan_index.html?schedule=dane';
     //Pobranie aktyalnej localstorage
     let fav_map = new Map(JSON.parse(localStorage.getItem('fav_plans') || '[]'));
 
     let documentContent = parent.document.getElementById('schedule-frame').contentDocument;
     let iframeUrl = documentContent.URL;
 
-    let lastPart = iframeUrl.substring(iframeUrl.lastIndexOf('/') + 1);
-    let fullUrl = `${urlBase}/${lastPart}`;
+    let last_part = iframeUrl.substring(iframeUrl.lastIndexOf('/') + 1);
+    let full_url = `${url_base}/${last_part}`;
     //pobranie nazwy planu
-    let plan_name = document.getElementById('plan_name');
+    let schedule_name_span = document.getElementById('schedule-name');
     //Dodanie/usunięcie planu z ulubionych
-    if(!fav_map.has(fullUrl)) {
-        fav_map.set(fullUrl, plan_name.innerText);
-    } else {
-        fav_map.delete(fullUrl);
-    }
+    if (!fav_map.has(full_url))
+        fav_map.set(full_url, schedule_name_span.innerText);
+    else
+        fav_map.delete(full_url);
     localStorage.setItem('fav_plans', JSON.stringify(Array.from(fav_map.entries())));
-    setTimeout(function(){
-        location.reload();
-    }, 600);
+
+    setTimeout(() => { location.reload(); }, 600);
 }
 
 function createTwitchEmbed(event)
